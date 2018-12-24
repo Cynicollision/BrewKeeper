5b9f4a9af93f88ee0a546d2b4ffac5fcdc74b37e
Sean Normoyle <Sean.Normoyle@financialpartners.com>
2018-12-24 09:52:17 -0500

    setting up CI

--- client/src/karma.conf.js
+++ client/src/karma.conf.js
@@ -1,5 +1,4 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+let env = (process.env.NODE_ENV || 'development');
 
 module.exports = function (config) {
   config.set({
@@ -13,7 +12,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
@@ -26,6 +25,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: env !== 'development', //single run in CI or production environments
   });
 };
