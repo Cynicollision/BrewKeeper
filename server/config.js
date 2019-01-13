6bb5b67961bc6a2022ef13fde756efc5acfe10ee
Sean Normoyle <stn825@gmail.com>
2019-01-13 10:54:33 -0500

    0.0.7

--- server/config.js
+++ server/config.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Config {
 }
 Config.isDev = (process.env.NODE_ENV || 'development') === 'development';
-Config.serverPort = 3000;
+Config.serverPort = process.env.PORT || 3000;
 exports.Config = Config;
 //# sourceMappingURL=config.js.map
