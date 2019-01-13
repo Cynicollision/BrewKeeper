801d4ff8447de38559ac571fcad260422f4cb901
Sean Normoyle <stn825@gmail.com>
2019-01-13 10:47:25 -0500

    0.0.6

---
+++ shared/contracts/OperationResponse.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class EmptyOperationResponse {
+    constructor() {
+        this.success = false;
+    }
+}
+exports.EmptyOperationResponse = EmptyOperationResponse;
+class OperationResponse extends EmptyOperationResponse {
+}
+exports.OperationResponse = OperationResponse;
+//# sourceMappingURL=OperationResponse.js.map
