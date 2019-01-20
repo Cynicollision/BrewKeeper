17da9dd3a79194bf7f909a0f00f3ac4e03968770
Sean Normoyle <stn825@gmail.com>
2019-01-20 15:24:06 -0500

    build

--- server/config.ts
+++ server/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-    static readonly mongo = process.env.MONGODB_URI || 'mongodb://localdev:l0cald3v@localhost:27017/brewkeeper'
+    static readonly mongo = process.env.MONGODB_URI || 'mongodb://heroku_plkzjqr5:v0fncearcvi9akue09see6jhtq@ds255794.mlab.com:55794/heroku_plkzjqr5'
     static readonly dev = (process.env.NODE_ENV || 'development') === 'development';
     static readonly port = process.env.PORT || '3000';
 }
