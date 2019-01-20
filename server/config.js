3fdc9f051cd3160c95c0060c90cc9db2d3b4d867
Sean Normoyle <stn825@gmail.com>
2019-01-20 15:38:24 -0500

    build

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Config {
 }
-Config.mongo = process.env.MONGODB_URI || 'mongodb://localdev:l0cald3v@localhost:27017/brewkeeper';
+Config.mongo = process.env.MONGODB_URI || 'mongodb://heroku_plkzjqr5:v0fncearcvi9akue09see6jhtq@ds255794.mlab.com:55794/heroku_plkzjqr5';
 Config.dev = (process.env.NODE_ENV || 'development') === 'development';
 Config.port = process.env.PORT || '3000';
 exports.Config = Config;
